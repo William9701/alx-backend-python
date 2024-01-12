f096b5c773bb6f42f000a300bf77e1d96faa1825
Obi William <williamobi818@gmail.com>
2024-01-12 08:03:41 +0100

    updated no 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,5 +4,6 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """This method returns the tuple of a string and the sqr"""
     sqr = v * v
     return k, sqr
