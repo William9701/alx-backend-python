fed0cf65de9f1751b29eb32c941f7ce4cecf17e0
Obi William <williamobi818@gmail.com>
2024-01-17 02:06:53 +0100

    python_async_comprehension- update

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """This is an asynchronous generator module"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """This method  that takes no arguments, loops 10 times, each time
     asynchronously waits 1 second, then yields a random number between 0
     and 10"""
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 """This is an asynchronous generator module"""
-
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """This method takes no arguments, collects 10 random numbers using
-    an async comprehensing over async_generator, then returns the 10
+    an async comprehension over async_generator, then returns the 10
     random numbers."""
-    random_numbers = []
-    # Using 'async for' to iterate over the async_generator
-    async for value in async_generator():
-        random_numbers.append(value)
-    return random_numbers
+    return [i async for i in async_generator()]
